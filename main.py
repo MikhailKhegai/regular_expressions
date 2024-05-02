1f0243638f89dc3debbef6dfbe3aa14da69ea80d
Mikhail Khegai <khegai200529@gmail.com>
2024-05-02 16:06:57 +0300

    init commit

--- main.py
+++ main.py
@@ -28,13 +28,26 @@ with open('C:\\Users\\Mario\\Downloads\\phonebook_raw.csv', encoding="utf-8") as
     pattern = r'(\+7|8)\s*\(*(\d{1,3})\)*\-*\s*(\d{1,3})\-*(\d{1,2})\-*(\d+)(\s*)\(*((\w+\.)\s(\w+)\)*)*'
     sentence[5] = re.sub(pattern, r'+7\(\2\)\3\-\4\-\5\6\8\9', sentence[5])
 
+dictionary = {}
+for d in new_contact_list:
+  if ' '.join(d[:2]) not in dictionary:
+    dictionary[' '.join(d[:2])] = d[2:]
+  else:
+    for i in range(len(dictionary[' '.join(d[:2])])):
+      if dictionary[' '.join(d[:2])][i] == '':
+        dictionary[' '.join(d[:2])][i] = d[2:][i]
+
+newest_list = []
+
+for key, value in dictionary.items():
+  newest_list.append(list(key.split(' ')) + value)
 
 
-print(new_contact_list)
+
+
 with open("new_phonebook_raw.csv", "w", encoding="utf-8") as f:
   datawriter = csv.writer(f, delimiter=',')
-  datawriter.writerows(new_contact_list)
-
+  datawriter.writerows(newest_list)
 
 
 
